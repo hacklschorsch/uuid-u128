2a6d877f213328153c08c0f699316bd4e32918e9
Florian Sesser <florian@leastauthority.com>
2024-03-10 18:15:11 +0000

    Lint

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { Buffer } from 'node:buffer'
 import * as uuid from 'uuid'
 
 function uuidv4StrToU128(uuidStr) {
--- index.test.js
+++ index.test.js
@@ -3,15 +3,15 @@ import assert from 'node:assert'
 import * as uuid from 'uuid'
 import { uuidv4StrToU128, u128ToUuidv4 } from './index.js'
 
-test('uuid.NIL (all zeros) -> 0u128', (t) => {
+test('uuid.NIL (all zeros) -> 0u128', () => {
     assert.strictEqual(uuidv4StrToU128(uuid.NIL), '0u128')
 })
 
-test('0u128 -> uuid.NIL (all zeros)', (t) => {
+test('0u128 -> uuid.NIL (all zeros)', () => {
     assert.strictEqual(u128ToUuidv4('0u128'), uuid.NIL)
 })
 
-test('Poor man\'s QuickCheck / parametric e2e test', (t) => {
+test('Poor man\'s QuickCheck / parametric e2e test', () => {
     for (let i = 0; i < 1_000; i++) {
         let input = uuid.v4()
         let output = u128ToUuidv4(uuidv4StrToU128(input))
